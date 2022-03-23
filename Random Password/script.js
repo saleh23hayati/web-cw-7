67968562b9265dbd282bea00ab07145cafe1c6a8
saleh23hayati <shayati2003.uk@gmail.com>
2022-03-23 16:51:16 +0000

    done

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,3 +1,44 @@
-// Database قاعدة البيانات
 
-// دالة generate
+
+ const input = document.querySelector("input");
+ 
+ const button = document.querySelector("button");
+ 
+
+ 
+   {Number} [length] 
+ {String} 
+ 
+ function GeneratePassword(length = 16) {
+   
+   
+    {String}
+    
+   const charset =
+     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+ 
+   
+    {String}
+   
+   let password = "";
+ 
+  
+   for (let i = 0; i < length; ++i) {
+     
+     {Number}
+      
+     let at = Math.floor(Math.random() * (charset.length + 1));
+ 
+     
+     password += charset.charAt(at);
+   }
+ 
+  
+   return password;
+ }
+ 
+ 
+ button.addEventListener("click", () => {
+   input.value = GeneratePassword(16);
+ });
+ 
