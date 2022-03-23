61f1064d85805f3c62427fb6f2126e03bd4dd568
saleh23hayati <shayati2003.uk@gmail.com>
2022-03-23 15:19:36 +0000

    done

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,19 @@
 // project أنشئ كائن
+const project = {
+type: "tech",
+users: 1000,
+biography() {
+    return "this is my project"
+}
 
+}
 // randomProperty أنشئ دالة
+function randomProperty(obj) {
+const keys = object.keys(obj);
+const random = Math.floor(Math.random() * keys.length);
+console.log(keys[random]);
+console.log(obj[`${keys[random]}`]);
+
+
+}
+randomProperty(project);
